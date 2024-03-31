74b41801e47232596f338122899f736108c2ebd7
Jason-6996 <umidjon95@gmail.com>
2024-03-31 14:53:55 +0900

    feat: Couple of NodeJs exercises and tiny projects

---
+++ app.js
@@ -0,0 +1,8 @@
+const math = require("./math");
+const cats = require("./shelter");
+
+// console.log(math.PI);
+
+// console.log(math.square(9));
+
+console.log("Required an entire directory", cats);
